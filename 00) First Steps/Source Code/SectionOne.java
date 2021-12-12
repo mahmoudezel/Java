49985d2d032f27197a5b9c5e73fcb9fb46996c63
MahmoudEzel <35092476+mahmoudezel@users.noreply.github.com>
2021-12-13 00:41:14 +0200

    Add files via upload

---
+++ 00) First Steps/Source Code/SectionOne.java
@@ -0,0 +1,10 @@
+// class name section one which prints some two statments
+public class SectionOne {
+// main method which is the first method to be compiled
+public static void main(String[] args) {
+// prints "Hello World" in line and then changes the cursor to the next line
+   System.out.println("Hello World");
+// prints "Hello Java!" in line and then changes the cursor to the next line
+   System.out.println("Hello Java!");
+   }
+}
